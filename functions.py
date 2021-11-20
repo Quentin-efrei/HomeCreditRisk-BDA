9bc008591bef4702fbb8db82261509888f40f502
guillaume <guillaume.jaouen@efrei.net>
2021-11-20 10:46:48 +0100

    Ajout des logs pour faire fonctionner MLFlow

--- functions.py
+++ functions.py
@@ -104,5 +104,12 @@ def accuracy(predictions,y_test):
     '''
     Function that print performance of a model based on validation dataset
     '''
+    return metrics.accuracy_score(predictions,y_test) #printing accuracy of model
     print('Accuracy of model : ',metrics.accuracy_score(predictions,y_test)) #printing accuracy of model
 
+def fetch_logged_data(run_id):
+    client = mlflow.tracking.MlflowClient()
+    data = client.get_run(run_id).data
+    tags = {k: v for k, v in data.tags.items() if not k.startswith("mlflow.")}
+    artifacts = [f.path for f in client.list_artifacts(run_id, "model")]
+    return data.params, data.metrics, tags, artifacts
--- training-models.py
+++ training-models.py
@@ -1,6 +1,8 @@
 import sys
 from sklearn.model_selection import train_test_split
 from functions import pd,scale_data,train_random_forest,predictions,accuracy,train_xgboost,train_gradient_boosting
+import mlflow
+import mlflow.sklearn
 
 import pickle
 
@@ -12,27 +14,39 @@ def main(argv):
     X = df.drop('TARGET',axis=1)
     X = scale_data(X)
     X_train, X_test, Y_train,Y_test = train_test_split(X,Y,random_state=42,test_size=0.2)
-    if argv[1] == 'RandomForest':
-        rforest = train_random_forest(X_train,Y_train)
-        pred = predictions(rforest,X_test)
-        acc = accuracy(pred,Y_test)
-        filename = 'models/randomforest.sav'
-        pickle.dump(rforest,open(filename,'wb'))
-    elif argv[1] == 'XGBoost':
-        xgbo = train_xgboost(X_train,Y_train)
-        pred = predictions(xgbo,X_test)
-        acc = accuracy(pred,Y_test)
-        filename = 'models/xgboostclassifier.sav'
-        pickle.dump(xgbo,open(filename,'wb'))
-    elif argv[1] == 'GradientBoostingClassifier':
-        gdb = train_gradient_boosting(X_train,Y_train)
-        pred = predictions(gdb,X_test)
-        acc = accuracy(pred,Y_test)
-        filename = 'models/gdbclassifier.sav'
-        pickle.dump(gdb,open(filename,'wb'))
-    else:
-        print('error')
-        sys.exit(2)
+
+    mlflow.sklearn.autolog()
+    mlflow.xgboost.autolog()
+
+    with mlflow.start_run():
+
+
+
+        if argv[1] == 'RandomForest':
+            rforest = train_random_forest(X_train,Y_train)
+            pred = predictions(rforest,X_test)
+            acc = accuracy(pred,Y_test)
+            metrics = mlflow.sklearn.eval_and_log_metrics(rforest, X_test, Y_test, prefix="val_")
+            mlflow.sklearn.log_model(rforest, "model_random_forest")
+            filename = 'models/randomforest.sav'
+            pickle.dump(rforest,open(filename,'wb'))
+        elif argv[1] == 'XGBoost':
+            xgbo = train_xgboost(X_train,Y_train)
+            pred = predictions(xgbo,X_test)
+            acc = accuracy(pred,Y_test)
+            metrics = mlflow.sklearn.eval_and_log_metrics(xgbo, X_test, Y_test, prefix="val_")
+            filename = 'models/xgboostclassifier.sav'
+            pickle.dump(xgbo,open(filename,'wb'))
+        elif argv[1] == 'GradientBoostingClassifier':
+            gdb = train_gradient_boosting(X_train,Y_train)
+            pred = predictions(gdb,X_test)
+            acc = accuracy(pred,Y_test)
+            metrics = mlflow.sklearn.eval_and_log_metrics(gdb, X_test, Y_test, prefix="val_")
+            filename = 'models/gdbclassifier.sav'
+            pickle.dump(gdb,open(filename,'wb'))
+        else:
+            print('error')
+            sys.exit(2)
 
 
 if __name__ == "__main__":
